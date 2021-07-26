d0b16743c56fc1b8fc947e092997660b14858a94
Roberto Luiz <luizroberto1957@hotmail.com>
2021-07-26 20:44:38 -0300

    feature: Mudança Layout/ Chamada da Pagina de Receitas

--- public/scripts.js
+++ public/scripts.js
@@ -1,29 +1,35 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const modal = document.querySelector('.modal');
+//const modalOverlay = document.querySelector('.modal-overlay');
+//const modal = document.querySelector('.modal');
 
 const cards = document.querySelectorAll('.card');
 
-const recipes = document.querySelector('.recipes');
-//const accordion = document.querySelector('.content_accordion');
-const accordionContent = document.querySelector('.content_accordion')
-
 const accordions = document.querySelectorAll('.recipes .accordions');
 
+const current = location.pathname;
+const links = document.querySelectorAll('.links a');
 
 for(let card of cards){
     card.addEventListener("click", function(){
         const postId = card.getAttribute("id")
+        //const postId = card.IndexOf(card.getAttribute("id"))
+        //console.log(card.IndexOf(card.getAttribute("id")))
         //modalOverlay.classList.add('active')
         //modalOverlay.querySelector(".image_container_modal img").src = `/assets/${postId}.png`
         //modalOverlay.querySelector(".image_container_modal img").src = card.querySelector(".card_image-container img").src
         //modalOverlay.querySelector(".title_modal p").textContent = card.querySelector(".card_content p").textContent
         //modalOverlay.querySelector(".info_modal p").textContent = card.querySelector(".card_info p").textContent
+        window.location.href = `recipes/${postId}`
     })
 }
-
-document.querySelector(".links a").addEventListener("click", function(){
-    document.querySelector(".sobre").style.fontWeight = "bold"
- })
+for(let link of links){
+    if (current.includes(link.getAttribute("href"))) {
+      //link.classList.add("active");
+      link.style.fontWeight = "bold";
+    }
+    if (current.includes("/recipes/") && link.textContent == "Receitas") {
+      link.style.fontWeight = "bold";
+    }
+}
 
 for(let accordion of accordions){
   accordion.addEventListener("click", function(){
@@ -37,17 +43,7 @@ for(let accordion of accordions){
     }
   })
 }
-/*
-document.querySelector(".recipes .title .accordion").addEventListener("click", function(){
-  if(accordion.className == "content_accordion hide"){ 
-    accordion.classList.remove("hide");
-    recipes.querySelector(".title a").textContent = "esconder"  
-  }else{
-    accordion.classList.add("hide");
-    recipes.querySelector(".title a").textContent = "mostrar"  
-  }
- })
-*/
+
  /*
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active");
