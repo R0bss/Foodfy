6057fec3af397e05a4beaa000f243ef3ac401f51
Roberto Luiz <luizroberto1957@hotmail.com>
2022-03-31 10:32:27 -0300

    chore: implementation edit recipe

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -31,7 +31,6 @@ exports.admin_index = function (req, res){
   return res.render("admin/index" , {items: data.recipes});
 }
 
-
 exports.admin_show = function (req, res) {
   const recipes = data.recipes; // Array de receitas carregadas do data.js
   const recipeIndex = req.params.id;
@@ -44,4 +43,28 @@ exports.admin_show = function (req, res) {
   }
   //console.log(recipes[recipeIndex]);
   
+
+}
+
+exports.admin_edit = function(req, res){
+  //req.params
+  const { id } = req.params
+  const recipes = data.recipes;
+/*
+  const foundMember = data.recipes.find(function(recipes) {
+    //return member.id == id
+    return recipes[id]
+  })
+  console.log(recipes[id]);
+*/
+console.log(recipes[id]);
+  if (!recipes[id]) return res.send("Member not found!")
+ 
+
+ const recipe ={
+    ...recipes[id],
+    //birth: date(recipes[id].birth).iso
+  }
+
+  return res.render('admin/edit', {recipe}) 
 }
