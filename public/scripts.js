f5f904218cff0d9623feb1a289a990998bda9179
Roberto Luiz <luizroberto1957@hotmail.com>
2022-04-02 18:19:03 -0300

    chore: Clean Code

--- public/scripts.js
+++ public/scripts.js
@@ -11,13 +11,6 @@ const links = document.querySelectorAll('.links a');
 for(let card of cards){
     card.addEventListener("click", function(){
         const postId = card.getAttribute("id")
-        //const postId = card.IndexOf(card.getAttribute("id"))
-        //console.log(card.IndexOf(card.getAttribute("id")))
-        //modalOverlay.classList.add('active')
-        //modalOverlay.querySelector(".image_container_modal img").src = `/assets/${postId}.png`
-        //modalOverlay.querySelector(".image_container_modal img").src = card.querySelector(".card_image-container img").src
-        //modalOverlay.querySelector(".title_modal p").textContent = card.querySelector(".card_content p").textContent
-        //modalOverlay.querySelector(".info_modal p").textContent = card.querySelector(".card_info p").textContent
         window.location.href = `recipes/${postId}`
     })
 }
@@ -43,17 +36,6 @@ for(let accordion of accordions){
     }
   })
 }
-
- /*
-document.querySelector(".close-modal").addEventListener("click", function(){
-    modalOverlay.classList.remove("active");
-    modalOverlay.querySelector(".image_container_modal img").src = ""
-        modalOverlay.querySelector(".title_modal p").textContent = ""
-        modalOverlay.querySelector(".info_modal p").textContent = ""
-
-})
-*/
-
 
 /* =================== ADMIN ==================== */
 const cardsAdmin = document.querySelectorAll('.card');
